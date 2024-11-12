db602537f8dd41f2a97438c25f877a8e51aa483c
Ilia Munaev <ilyamunaev@gmail.com>
2024-11-12 21:22:56 +0200

    Fead: func prorotype

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,14 @@
-
-#include "get_next_line.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imunaev- <imunaev-@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 20:39:00 by imunaev-          #+#    #+#             */
+/*   Updated: 2024/11/12 20:58:41 by imunaev-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 // ssize_t read(int fildes, void *buf, size_t nbyte);
 // If the value of nbyte is greater than {SSIZE_MAX}, the result is implementation-defined.
@@ -11,40 +20,53 @@
 ** Return: One line that was read, one line at a time,
 ** NULL if there is nothing else to read, or an error occurred.
 */
-char	*get_next_line(int fd)
+
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
 {
+    unsigned static char	main_buf[MAIN_BUF_SIZE]; // Static buffer for leftover data
+    unsigned char	*temp_buf;                       // Temporary buffer for reading in chunks
+    int	data;                                        // "Loop marker" for read status
+    char	*line;
+	int	head;
+	int	tail;
 
-	// static buffer for store data and work with it
-	unsigned static char	*buffer;
+	data = 1;
+	line = NULL;
+	head = 0;
+	tail = 0;
 
-	int	next_l_index;
+	//
 
-	// serves as marker for loop
-	int	line;
 
-	// allocate memory for the buffer (size_t for SSIZE_MAX)
-	buffer = allocate_memory(BUFFER_SIZE);
-	if (!buffer)
-		return (NULL);
+    // Allocate temporary buffer
+    temp_buf = malloc(BUFFER_SIZE);
+    if (!temp_buf)
+        return (NULL);
 
-	next_l_index = 0;
+    // Retrieve data until read() returns 0 (EOF)
+    while (data)
+    {
+        // Process any complete lines already in MAIN_BUF
+        while ((line = get_line(main_buf, head, tail)) != NULL)
+        {
+            free(temp_buf); // Free temp buffer if line is found
+            return (line);  // Return the extracted line
+        }
 
-	// retrive data untill read() return '0' (EOF)
-	// if line found return line, else return '0'
-	while (line)
-	{
-		// iterate through the buffer and find a line
-		line = get_line(buffer, next_l_index);
+        // Read from fd into TEMP_BUF, returns number of bytes read or 0 for EOF
+        data = read(fd, temp_buf, BUFFER_SIZE);
 
-		// adjust the index from which next itaration
-		next_l_index += len_line(line); // 
+        if (data > 0)
+            add_data(temp_buf, main_buf, head, tail); // Append TEMP_BUF contents to MAIN_BUF
+        else
+            return get_last_line(main_buf); // Return last line if no more data
+    }
 
-		return (line);
-	}
+    // Free allocated memory and clean up
+    free(temp_buf);
+    clean_up(main_buf); // Reset or clear MAIN_BUF as needed
 
-	// free memory allocated for the buffer
-	clean_up(buffer);
-
-	// nothing to read
-	return (NULL);
+    return NULL; // Return NULL to indicate EOF or no more data
 }
