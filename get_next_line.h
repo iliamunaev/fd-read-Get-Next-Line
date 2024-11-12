dcdb993fdce47dea5dc112c3e70d25ef53104878
Ilia Munaev <ilyamunaev@gmail.com>
2024-11-12 21:23:26 +0200

    Feat: assign buffer sizes

--- get_next_line.h
+++ get_next_line.h
@@ -1,15 +1,20 @@
-// get_next_line.h
-
-#ifndef GET_NEXT_LINE
-#define GET_NEXT_LINE
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
 
 #include <unistd.h> // for read()
 #include <stdlib.h> // for malloc(), free()
 
-// default buffer size
-#define BUFFER_SIZE 4096
+// Set default BUFFER_SIZE
+#ifndef BUFFER_SIZE
+    #define BUFFER_SIZE 4096
+#elif BUFFER_SIZE != 4096
+    #undef BUFFER_SIZE
+    #define BUFFER_SIZE 4096
+#endif
 
-// utils
+#define MAIN_BUF_SIZE 8192
+
+// Function prototype
 char	*get_next_line(int fd);
 
-#endif /* GET_NEXT_LINE */
+#endif /* GET_NEXT_LINE_H */
