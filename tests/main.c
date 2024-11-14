f5cc9cf73ed93f896f9fd6b31e99380b95016d1a
Ilia Munaev <imunaev-@c1r5p13.hive.fi>
2024-11-14 11:48:11 +0200

    Feat: tests upgrated, added more texts files

--- tests/main.c
+++ tests/main.c
@@ -4,13 +4,20 @@
 #include <fcntl.h>
 #include "../get_next_line.h"
 
+// multiple_empty_lines.txt
+// multiple_long_lines.txt
+// multiple_short_lines.txt
+// single_empty_line.txt
+// single_long_line.txt
+// single_short_line.txt
 
 int main(void)
 {
 	int fd;
 	char *line;
 
-	fd = open("text_for_tests.txt", O_RDONLY);
+	// ------- REPLACE TEXT FILE HERE -------
+	fd = open("single_short_line.txt", O_RDONLY);
 	if (fd == -1)
 	{
 	perror("Error opening file");
